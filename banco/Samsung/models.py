0dbe58063f97cd5ad2d2e240a70cbddffc2e84ca
Esther <tetececilia231@gmail.com>
2020-02-25 16:13:03 -0300

    Organiza models

--- banco/Samsung/models.py
+++ banco/Samsung/models.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1252 -*-
 from sqlalchemy import create_engine, Column,Integer, String, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,7 +15,7 @@ class Sintomas(Base):
     nome = Column(String(80))
 
     def __repr__(self):
-        return '<sintoma {}>'.format(self.nome)
+        return '<sintoma: {}>'.format(self.nome)
 
     def save(self):
         db_session.add(self)
@@ -23,6 +24,25 @@ class Sintomas(Base):
     def delete(self):
         db_session.delete(self)
         db_session.commit()
+        
+
+class Prevencoes(Base):
+    __tablename__='prevencoes'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(80))
+
+    def __repr__(self):
+        return '<prevenção: {}>'.format(self.nome)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+
 
 
 class Doencas(Base):
@@ -31,10 +51,12 @@ class Doencas(Base):
     nome = Column(String(40), index=True)
     sintoma_id = Column(Integer, ForeignKey('sintomas.id'))
     sintoma = relationship("Sintomas")
+    prevencao_id = Column(Integer, ForeignKey('prevencoes.id'))
+    prevencao = relationship("Prevencoes")
 
 
     def __repr__(self):
-        return '<doenca {}>'.format(self.nome)
+        return '<doença: {}>'.format(self.nome)
 
     def save(self):
         db_session.add(self)
@@ -43,6 +65,51 @@ class Doencas(Base):
     def delete(self):
         db_session.delete(self)
         db_session.commit()
+
+
+class Salas(Base):
+    __tablename__='salas'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40))
+    senha = Column(String(40))
+    publica = Column(String(10))
+    
+
+
+    def __repr__(self):
+        return '<sala: {}>'.format(self.nome)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+
+
+class Sessoes(Base):
+    __tablename__='sessoes'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40), index=True)
+    doenca_id = Column(Integer, ForeignKey('doencas.id'))
+    doenca = relationship("Doencas")
+    sala_id = Column(Integer, ForeignKey('salas.id'))
+    sala = relationship("Salas")
+
+
+    def __repr__(self):
+        return '<doença: {}>'.format(self.nome)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
 
 
 def init_db():
